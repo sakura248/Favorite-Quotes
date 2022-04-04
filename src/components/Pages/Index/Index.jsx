36b4ecb35316952f59d007e7d9f4a0d16fa365b5
Sakura Nishiya <73939499+sakura248@users.noreply.github.com>
2022-04-04 14:19:07 -0700

    sorting modified

--- src/components/Pages/Index/Index.jsx
+++ src/components/Pages/Index/Index.jsx
@@ -1,21 +1,19 @@
 /* eslint-disable no-unused-vars */
 import React, { useState } from "react";
-// import SearchBar from "../../Filtering/SearchBar/SearchBar";
 import Sort from "../../Filtering/Sort/Sort";
 import Hero from "../../Hero/Hero";
 import QuotesList from "../../QuotesList";
 
 function Index() {
   const [order, setOrder] = useState("newest");
-
   const handleChange = (selectedOrder) => {
     setOrder(selectedOrder.value);
   };
+
   return (
     <div>
       <Hero />
       <div className="flex flex-row justify-end mr-8">
-        {/* <SearchBar /> */}
         <Sort onChange={handleChange} />
       </div>
       <QuotesList type="all" order={order} />
